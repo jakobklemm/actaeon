d3fa435b5b993217f45350938e04bc128a26a166
Jakob Klemm <github@jeykey.net>
2021-09-03 12:27:02 +0200

    Added initial addr impl.

--- src/node.rs
+++ src/node.rs
@@ -16,20 +16,81 @@
 //! In addition each node also contains other fields like timestamps
 //! and (in the future) a cache of recent messages.
 
+use crate::error::Error;
+use sodiumoxide::crypto::box_::curve25519xsalsa20poly1305::{PublicKey, SecretKey};
 use std::time::SystemTime;
 
-struct Node<'a> {
+struct Node {
     timestamp: SystemTime,
-    address: Address<'a>,
+    address: Address,
     connection: Connection,
 }
 
-/// Current short hand for the self node, used for calculating the distance in the Ord impl.
-struct Center([u8; 32]);
-
-struct Address<'a> {
-    id: [u8; 32],
-    center: &'a Center,
+/// Config for self / this node, currently as part of the Node module,
+/// might get restructured into a dedicated module in the future,
+/// should it increase in scope. It has to be created before all other
+/// nodes and stored in the interface.
+struct Center {
+    public: PublicKey,
+    secret: SecretKey,
+    uptime: SystemTime,
 }
 
-struct Connection {}
+struct Address {
+    key: PublicKey,
+}
+
+struct Connection {
+    ip: String,
+    port: usize,
+}
+
+impl Address {
+    /// Create a new Address from a public key. (currently the only
+    /// field so it could be created manually.) This is usally not
+    /// meant to be used by the user / externally.
+    fn new(public: PublicKey) -> Self {
+        Self { key: public }
+    }
+
+    /// Most of the times addresses will be created from bytes coming
+    /// over the network, this function can be used, although it might
+    /// fail if the key is invalid.
+    fn from_bytes(bytes: [u8; 32]) -> Result<Self, Error> {
+        if let Some(public) = PublicKey::from_slice(&bytes) {
+            Ok(Self { key: public })
+        } else {
+            Err(Error::Invalid(String::from("public key is invalid")))
+        }
+    }
+
+    fn as_bytes(&self) -> [u8; 32] {
+        let mut bytes: [u8; 32] = [0; 32];
+        let key = self.key.as_ref();
+        for (i, j) in key.into_iter().enumerate() {
+            bytes[i] = *j;
+        }
+        return bytes;
+    }
+
+    /// If a random Address is required this can generate a public key
+    /// from an input string by hashing it.
+    fn generate(source: &str) -> Result<Self, Error> {
+        let bytes = blake3::hash(source.as_bytes()).as_bytes().to_owned();
+        Address::from_bytes(bytes)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use sodiumoxide::crypto::box_;
+
+    #[test]
+    fn test_address_from_bytes() {
+        let (p, _s) = box_::gen_keypair();
+        let real = Address::new(p.clone());
+        let test = Address::from_bytes(p.0).unwrap();
+        assert_eq!(real.key.0, test.key.0);
+    }
+}
